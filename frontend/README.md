eafa3afc33702ed570511b513b2064c38c06cd61
Dimas <dimasagungprayoga143@gmail.com>
2023-08-01 21:07:10 +0700

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,7 @@
+# React + TypeScript + Vite
+
+## Lesson 
+
+1. Introduction
+2. Install Tools 
+3. Create TypeScript React App By Vite
---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import './App.css'
+
+function App() {
+    const [cout, setCout] = useState(0)
+
+    return (
+        <div>
+            <header>Babyshark</header>
+            <main></main>
+            <footer>All right reserved</footer>
+        </div>
+    )
+}
+
+export default App
